f113c8de69503a55069fc220e8b9ad8ef52c0b37
Sharon Zou <szou00@stuy.edu>
2020-06-23 10:44:43 -0400

    Initial Commit

---
+++ ToDoListApp/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoListApp
+//
+//  Created by Sharon Zou on 6/22/20.
+//  Copyright © 2020 Sharon Zou. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false 
+}
---
+++ ToDoListApp/ToDoTableViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoListApp
+//
+//  Created by Sharon Zou on 6/22/20.
+//  Copyright © 2020 Sharon Zou. All rights reserved.
+//
+
+// 
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+    
+    var toDos : [ToDoCD] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        toDos = createToDos()
+//        getToDos()
+    }
+
+    // MARK: - Table view data source
+    
+    func createToDos() -> [ToDo] {
+        let swift = ToDo()
+        swift.name = "Learn Swift"
+        swift.important = true
+        
+        let dog = ToDo()
+        dog.name = "Walk the Dog"
+        //important is set to false by default
+        
+        return [swift,dog]
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+      let toDo = toDos[indexPath.row]
+
+      if let name = toDo.name {
+        if toDo.important {
+            cell.textLabel?.text = "❗️" + name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+      }
+
+      return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let addVC = segue.destination as? AddToDoViewController {
+            addVC.previousVC = self
+        }
+        
+        if let completeVC = segue.destination as? CompleteToDoViewController {
+            if let toDo = sender as? ToDoCD {
+                completeVC.selectedToDo = toDo
+                completeVC.previousVC = self
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+      // this gives us a single ToDo
+      let toDo = toDos[indexPath.row]
+
+      performSegue(withIdentifier: "moveToComplete", sender: toDo)
+    }
+    
+    func getToDos() {
+      if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+
+        if let coreDataToDos = try? context.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                toDos = coreDataToDos
+                tableView.reloadData()
+        }
+      }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      getToDos()
+    }
+
+}
